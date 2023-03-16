296b96b3d683f52f6d8c147f9e1891222d6fc1c6
Leonardo Kiszewski <kiszewski1999@gmail.com>
2023-03-16 16:17:44 -0300

    Add display page

---
+++ lib/pages/display_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DisplayPage extends StatefulWidget {
+  const DisplayPage({super.key});
+
+  @override
+  State<DisplayPage> createState() => _DisplayPageState();
+}
+
+class _DisplayPageState extends State<DisplayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
